efccace981732ac3d95c0b1dda478640e94aea80
ashen1queter <mnojasish@gmail.com>
2025-07-18 02:09:14 +0530

    Feeling a lot tired lately. I couldnt do much

---
+++ examples/jesica/ble_app_blinky - Copy/pca10059/s140/ses/Output/Debug/Obj/ble_app_blinky_pca10059_s140/ses_startup_nrf52840.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ble_app_blinky_pca10059_s140/ses_startup_nrf52840.o: \
+ C:\Users\HI\Desktop\pikaaa\nRF52_dongle\modules\nrfx\mdk\ses_startup_nrf52840.s
---
+++ examples/jesica/ble_app_blinky - Copy/pca10059/s140/ses/Output/Debug/Obj/ble_app_blinky_pca10059_s140/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ble_app_blinky_pca10059_s140/ses_startup_nrf_common.o: \
+ C:\Users\HI\Desktop\pikaaa\nRF52_dongle\modules\nrfx\mdk\ses_startup_nrf_common.s
